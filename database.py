76853b2cfbcaaa82d2292d13dfa9b5663ffcb901
Immanuel ifeoluwa <ifeoluwaimmanuel004@gmail.com>
2025-08-25 10:22:57 +0100

    fix database url

--- database.py
+++ database.py
@@ -1,9 +1,25 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql://postgres:123456@localhost:5432/fast_api_project"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+
+if not DATABASE_URL:
+    raise ValueError("❌ No DATABASE_URL found. Did you set it in Render?")
+
+
+
+
+    
+    
+
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
